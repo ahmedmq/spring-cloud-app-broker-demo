4c0bd4a39be665ff30b6b25ac2e6370fdf070be7
Mushtaq Ahmed <amushtaq@vmware.com>
2020-09-02 13:36:48 +0530

    Initial Commit...

---
+++ sample-app-broker/src/test/java/org/springframework/cloud/appbroker/samples/broker/SampleAppBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.cloud.appbroker.samples.broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleAppBrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ sample-app-service/src/main/java/org/springframework/cloud/appbroker/samples/service/SampleAppServiceApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.cloud.appbroker.samples.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleAppServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleAppServiceApplication.class, args);
+	}
+
+}
